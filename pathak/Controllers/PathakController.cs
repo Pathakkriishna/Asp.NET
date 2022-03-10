6021aca8819ee0f3c0a5fb4e384695c5f74b8cea
KIST Student3 <student3@sddb.student>
2022-03-10 09:23:49 +0545

    initial commit

---
+++ pathak/Controllers/PathakController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pathak.Controllers
+{
+    public class PathakController : Controller
+    {
+        // GET: Pathak
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
